95461671aa4bc8315b68a89a31779fc8470cd5ac
JirachiTheEpic <jirachibutemail@gmail.com>
2026-04-07 18:40:22 -0700

    Update charlie.py

--- charlie.py
+++ charlie.py
@@ -29,20 +29,20 @@ def post_comment(project_id, comment_text):
     r = requests.post(url, json=payload, headers=headers)
 
     if r.status_code == 200:
-        print("charlie did it", comment_text)
+        print("charlie did it:", comment_text)
     else:
-        print("charlie sad")
+        print("charlie sad:", r.status_code, r.text)
 
 
 if __name__ == "__main__":
-    print("charlie v1.0")
+    print("charlie v1.1")
 
-    project_id = input("id").strip()
-    count = int(input("how many").strip())
-    delay = float(input("seconds delay").strip())
+    project_id = input("id: ").strip()
+    count = int(input("how many: ").strip())
+    delay = float(input("seconds delay: ").strip())
 
     print("what the charlie do you wanna comment")
-    print("leave blank for charlie")
+    print("leave blank for charlie timestamp mode\n")
 
     for i in range(count):
         comment_text = input(f"Comment #{i+1}: ").strip()
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         post_comment(project_id, comment_text)
 
         if i < count - 1:
-            print("waiting {delay} seconds for charlie")
+            print(f"waiting {delay} seconds for charlie…")
             time.sleep(delay)
 
     print("charlie has charlied")
